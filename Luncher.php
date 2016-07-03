df7344ecbdb87ca338674244d260a0357b90ef41
sina ebrahimi <sina021370@gmail.com>
2016-07-04 00:10:24 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '266788645:AAHpaRhniHSSdHy8lfE0SSDWKxxDEWFg7I0');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
